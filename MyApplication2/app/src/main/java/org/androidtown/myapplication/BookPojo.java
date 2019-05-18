a3fa017cf2ff2ea9645fff6c8f74a5198e6c4fb9
chlgpdus921 <chlgpdus921@naver.com>
2019-05-19 02:58:46 +0900

    filemanage
    fileManagement

---
+++ MyApplication2/app/src/main/java/org/androidtown/myapplication/BookPojo.java
@@ -0,0 +1,46 @@
+package org.androidtown.myapplication;
+
+public class BookPojo {
+    private String name;
+    private String day;
+    private boolean folder;
+    private String size;
+
+    public BookPojo(String name, boolean folder) {
+        this.name = name;
+        this.folder = folder;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFolder() {
+        return folder;
+    }
+
+    public void setFolder(boolean folder) {
+        this.folder = folder;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+}
